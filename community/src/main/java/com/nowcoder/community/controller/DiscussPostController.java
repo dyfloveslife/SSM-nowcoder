50e49aba08f421a850ab29a83865bd986fb96227
dyfloveslife <786616590@qq.com>
2020-01-29 20:47:03 +0800

    add discuss post feature

--- community/src/main/java/com/nowcoder/community/controller/DiscussPostController.java
+++ community/src/main/java/com/nowcoder/community/controller/DiscussPostController.java
@@ -3,10 +3,13 @@ package com.nowcoder.community.controller;
 import com.nowcoder.community.entity.DiscussPost;
 import com.nowcoder.community.entity.User;
 import com.nowcoder.community.service.DiscussPostService;
+import com.nowcoder.community.service.UserService;
 import com.nowcoder.community.utils.CommunityUtil;
 import com.nowcoder.community.utils.HostHolder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -22,6 +25,9 @@ public class DiscussPostController {
 
     @Autowired
     private HostHolder hostHolder;
+
+    @Autowired
+    private UserService userService;
 
     @RequestMapping(path = "/add", method = RequestMethod.POST)
     @ResponseBody  // 返回的是字符串
@@ -43,5 +49,20 @@ public class DiscussPostController {
         discussPostService.addDiscussPost(post);
         // 报错情况，将来统一处理
         return CommunityUtil.getJSONString(0, "发送成功!");
+    }
+
+    // 查询帖子的时候需要根据 id 查，这里需要把 id 传进来
+    // 在路径中取值，需要用到 @PathVariable 注解
+    // 使用 model 携带一些数据
+    @RequestMapping(path = "/detail/{discussPostId}", method = RequestMethod.GET)
+    public String getDiscussPost(@PathVariable("discussPostId") int discussPostId, Model model) {
+        // 获得帖子
+        DiscussPost post = discussPostService.findDiscussPostById(discussPostId);
+        model.addAttribute("post", post);
+        // 获得作者
+        User user = userService.findUserById(post.getUserId());
+        model.addAttribute("user", user);
+
+        return "/site/discuss-detail";
     }
 }
--- community/src/main/java/com/nowcoder/community/service/DiscussPostService.java
+++ community/src/main/java/com/nowcoder/community/service/DiscussPostService.java
@@ -41,5 +41,7 @@ public class DiscussPostService {
         return discussPostMapper.insertDiscussPost(post);
     }
 
-
+    public DiscussPost findDiscussPostById(int id) {
+        return discussPostMapper.selectDiscussPostById(id);
+    }
 }
